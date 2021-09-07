7293f695067bcd429ab37d3af8a93bd6526e1611
Nicolas Tito <mrnicolastito@icloud.com>
2021-09-07 17:02:57 +0100

    update

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: nide-mel <nide-mel@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 16:01:03 by nide-mel          #+#    #+#             */
-/*   Updated: 2021/09/07 17:00:09 by nide-mel         ###   ########.fr       */
+/*   Updated: 2021/09/07 17:02:43 by nide-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,11 +54,21 @@ static void	send_msg(int pid, char *msg)
 
 int	main(int ac, char **av)
 {
+	struct sigaction	s_sigaction;
+
 	if (ac != 3)
 	{
 		ft_putstr_fd("!!!ERROR!!!\nINAVALID ARGUMENT!\n", 1);
 		return (0);
 	}
+	ft_putstr_fd("Sent    : ", 1);
+	ft_putnbr_fd(ft_strlen(av[2]), 1);
+	ft_putchar_fd('\n', 1);
+	ft_putstr_fd("Received: ", 1);
+	s_sigaction.sa_handler = ft_sigaction;
+	s_sigaction.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
 	send_msg((int)ft_atoi(av[1]), av[2]);
 	return (0);
 }
