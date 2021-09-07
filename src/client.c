bbb6ab70ab664426e66b05babd697b6246f2ba70
Nicolas Tito <mrnicolastito@icloud.com>
2021-09-07 16:49:24 +0100

    Complete minitalk

--- src/client.c
+++ src/client.c
@@ -6,8 +6,59 @@
 /*   By: nide-mel <nide-mel@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 16:01:03 by nide-mel          #+#    #+#             */
-/*   Updated: 2021/09/06 16:15:58 by nide-mel         ###   ########.fr       */
+/*   Updated: 2021/09/07 16:47:37 by nide-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+static void	ft_sigaction(int sig)
+{
+	static int	i = 0;
+
+	if (sig == SIGUSR1)
+		++i;
+	else
+	{
+		ft_putnbr_fd(i, 1);
+		ft_putstr_fd("\n", 1);
+		exit(0);
+	}
+}
+
+static void	send_msg(int pid, char *msg)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = -1;
+	while (msg[++i])
+	{
+		while (++j < 8)
+		{
+			if (msg[i] & 0x80 >> j)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(100);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+int	main(int ac, char *av)
+{
+	if (ac != 3)
+	{
+		ft_putstr_fd("!!!ERROR!!!\nINAVALID ARGUMENT!\n", 1);
+		return (0);
+	}
+	send_msg(av[2], ft_atoi(av[1]));
+	return (0);
+}
--- src/server.c
+++ src/server.c
@@ -6,8 +6,51 @@
 /*   By: nide-mel <nide-mel@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 16:01:19 by nide-mel          #+#    #+#             */
-/*   Updated: 2021/09/06 16:16:05 by nide-mel         ###   ########.fr       */
+/*   Updated: 2021/09/07 16:42:13 by nide-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+static void	ft_sigaction(int sig, siginfo_t *sinfo, void *context)
+{
+	static int		i = 0;
+	static pid_t	cpid = 0;
+	static char		c = 0;
+
+	(void)context;
+	if (!cpid)
+		cpid = sinfo->si_pid;
+	c |= (sig, SIGUSR2);
+	if (++i == 8)
+	{
+		i = 0;
+		if (!c)
+		{
+			kill(cpid, SIGUSR2);
+			cpid = 0;
+			return ;
+		}
+		ft_putchar_fd(c, 1);
+		c = 0;
+		kill(cpid, SIGUSR1);
+	}
+	else
+		c <<= 1;
+}
+
+int	main(void)
+{
+	struct sigaction	s_sigaction;
+
+	ft_putstr_fd("PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putstr_fd("\n", 1);
+	s_sigaction.sa_sigaction = ft_sigaction;
+	s_sigaction.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
+	while (1)
+		pause();
+	return (0);
+}
